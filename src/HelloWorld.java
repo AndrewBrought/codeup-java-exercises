67b42ec9424a084b5f2bcb4e27ce30720695a6de
Andrew Brought <andrewbrought@gmail.com>
2020-06-01 12:20:29 -0500

    finished first exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        System.out.println("Hope you are well, I am excited to learn together!");
+    }
+}
--- src/Intro.java
+++ src/Intro.java
@@ -1,4 +1,4 @@
-public class intro {
+public class Intro {
     public static void main(String[] args){
         System.out.println("Hello, and welcome to intro to Java!");
     }
