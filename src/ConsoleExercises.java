4ad3f1b2cad06bea171e7a27890080fb01592e79
Andrew Brought <andrewbrought@gmail.com>
2020-06-02 15:16:28 -0500

    walkthrough notes on consoleIO exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -5,8 +5,8 @@ public class ConsoleExercises {
 //        double pi = 3.14159;
 //        System.out.printf("The value of pi is approximately %.2f.", pi);
 
-        Scanner sc = new Scanner(System.in);
-        sc.useDelimiter("\n");
+//        Scanner sc = new Scanner(System.in);
+//        sc.useDelimiter("\n");
 //        int userInput = sc.nextInt();
 //        System.out.println("You entered the following: " + userInput * 2);
 
@@ -48,14 +48,14 @@ public class ConsoleExercises {
 //        System.out.println("Please enter length and width of a classroom at Codeup: ");
 
 //        Variables storing user input
-        System.out.println("Please enter the length here: ");
+//        System.out.println("Please enter the length here: ");
+////
+//        int length = sc.nextInt();
+//        System.out.println("Please enter the width here: ");
+////
+//        int width = sc.nextInt();
 //
-        int length = sc.nextInt();
-        System.out.println("Please enter the width here: ");
-//
-        int width = sc.nextInt();
-
-        System.out.format("The area of the classroom per yours specs is: length: %dft * width: %dft" + " = " + length * width + " sqft", length, width);
+//        System.out.format("The area of the classroom per yours specs is: length: %dft * width: %dft" + " = " + length * width + " sqft", length, width);
 
 
 
@@ -73,6 +73,56 @@ public class ConsoleExercises {
 //
 //        System.out.format("The volume of the classroom per yours specs is: length: %.2fft * width: %.2fft * height: %.2fft" + " = " + length * width * height + " cubedft", length, width, height);
 
+//        ============== Walk Through Notes ===========
+
+//        System.out.println("Hello from Java!");
+//      Part 1.
+//        double pi = 3.14159;
+//        System.out.format("The value of pi is approximately %.2f.", pi);
+//
+//        Part 2
+//         Scanner sc = new Scanner(System.in);
+
+//        System.out.print("Enter an integer: ");
+//        int userInt = sc.nextInt();
+//        System.out.println("The user entered: " + userInt);
+
+//        Part 3
+//        System.out.println("Please enter three words...");
+//        String word1 = sc.next();
+//        String word2 = sc.next();
+//        String word3 = sc.next();
+//
+////        System.out.println(word1);
+////        System.out.println(word2);
+////        System.out.println(word3);
+//        System.out.printf("%s%n%s%n%s%n", word1, word2, word3);
+
+//        System.out.println("Please enter a sentence...");
+//        String sentence = sc.next(); //This doesnt work because it immediately jumps to the next line after the first word
+//        System.out.println(sentence);
+
+//        System.out.println("Please enter a sentence...");
+//        String sentence = sc.nextLine(); //This will allow the sentence on a given line to be recieved
+//        System.out.println(sentence);
+
+//        Part 4
+//        System.out.print("Please enter length of room in feet: ");
+//        String length = sc.nextLine();
+//        System.out.print("Please enter the width of the room in feet: ");
+//        String width = sc.nextLine();
+//        int area = Integer.parseInt(length) * Integer.parseInt(width);
+//        int perimeter = Integer.parseInt(length)  + Integer.parseInt(width) * 2;
+//        System.out.printf("The area of the room is %d%nThe perimeter of the room is %dft", area, perimeter);
+
+//        We would write it this way so that we can take the user's input from a string format and then convert...I suspect to prevent code from breaking
+//        System.out.print("Please enter length of room in feet: ");
+//        int length = Integer.parseInt(sc.nextLine());
+//        System.out.print("Please enter the width of the room in feet: ");
+//       int width = Integer.parseInt(sc.nextLine());
+//        int area = length * width;
+//        int perimeter = length  + width * 2;
+//        System.out.printf("The area of the room is %d%nThe perimeter of the room is %dft", area, perimeter);
 
 
     }
