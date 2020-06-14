0700004fed88f16120abc4bc48de572c00b15727
Andrew Brought <andrewbrought@gmail.com>
2020-06-14 16:55:25 -0500

    completed tomorrow's inerhitance and polymorphism exercise

---
+++ src/shapes/Square.java
@@ -0,0 +1,18 @@
+package shapes;
+
+public class Square extends Rectangle {
+
+    public Square(double side){
+        super(side,side);
+    }
+
+    public double getArea(){
+        return Math.pow(width, 2);
+    }
+
+    public double getPerimeter(){
+
+        return 4 * width;
+    }
+
+}
